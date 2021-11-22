d3f15ac421a0ffc2292fb335e53262100f3cb1fd
abazudosen <abudosen@gmail.com>
2021-11-22 12:07:37 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Platform } from "react-native";
+import * as Location from 'expo-location';
+import {withAuthenticator} from 'aws-amplify-react-native'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import Router from "./src/navigation/Root";
+
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
   },
 });
+
+const App = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        setErrorMsg(
+          'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+        );
+        return;
+      } else {
+        Location.requestForegroundPermissionsAsync();
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Router />
+    </>
+  );
+};
+
+export default withAuthenticator(App);
